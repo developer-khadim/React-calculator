20adab806ce0c745f87d304a5c09f6d18a834fce
Your Name <you@example.com>
2024-06-24 16:03:19 +0500

    add

--- app/page.js
+++ app/page.js
@@ -63,10 +63,10 @@ const Page = () => {
   return (
     <>
       <div className="flex justify-center items-center h-[100vh] bg-gray-100">
-        
         <div className="bg-black rounded-3xl p-4 w-72">
           <div className="flex flex-col justify-between mb-3">
-            <div className="text-blue-500  bg-opacity-20 p-1 rounded">
+            <div className="text-blue-500  bg-opacity-20 flex justify-between p-1 rounded">
+              <h1 className="font-semibold" >Khadim Ali</h1>
               <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
               </svg>
